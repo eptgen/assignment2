cbc8af0d6902ca1adf71ed41e259a228d5978e16
23jsr7 <evolene@cs.williams.edu>
2024-02-24 17:17:20 -0500

    mesh render

--- eval_model.py
+++ eval_model.py
@@ -30,10 +30,6 @@ def get_args_parser():
     return parser
 
 def preprocess(feed_dict, args):
-    feed_dict["mesh"] = feed_dict["mesh"].to(args.device)
-    for k in range(len(feed_dict["images"])):
-        feed_dict["images"][k] = feed_dict["images"][k].to(args.device)
-
     images = feed_dict['images'].squeeze(1)
     mesh = feed_dict['mesh']
     if args.load_feat:
